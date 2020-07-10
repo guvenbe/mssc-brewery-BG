cb53028894baa49a9d315d09b7f478dd4b8ba8c7
Bora Guven <guvenbe@gmail.com>
2020-07-10 13:58:04 -0400

    assignment

--- src/test/java/guru/springframework/msscbrewery/web/controler/BeerControllerTest.java
+++ src/test/java/guru/springframework/msscbrewery/web/controler/BeerControllerTest.java
@@ -1,33 +1,30 @@
 package guru.springframework.msscbrewery.web.controler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.msscbrewery.services.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-
-import java.math.BigDecimal;
-import java.time.OffsetDateTime;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.reset;
-
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
@@ -36,6 +33,12 @@ class BeerControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
 
     BeerDto validBeer;
 
@@ -46,6 +49,7 @@ class BeerControllerTest {
                 .id(UUID.randomUUID())
                 .beerName("Galaxy Cat")
                 .beerStyle("Pale Ale")
+                .upc(123456789012L)
                 .build();
     }
     @AfterEach
@@ -60,12 +64,42 @@ class BeerControllerTest {
         MvcResult result = mockMvc.perform(get("/api/v1/beer/" + validBeer.getId()))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id", is(validBeer.getId().toString())))
-                .andExpect(jsonPath("$.beerName", is(validBeer.getBeerName())))
-                .andExpect(jsonPath("$.beerStyle", is(validBeer.getBeerStyle())))
+                .andExpect(jsonPath("$.id", Matchers.is(validBeer.getId().toString())))
+                .andExpect(jsonPath("$.beerName", Matchers.is(validBeer.getBeerName())))
+                .andExpect(jsonPath("$.beerStyle", Matchers.is(validBeer.getBeerStyle())))
                 .andReturn();
 
         System.out.println(result.getResponse().getContentAsString());
+
+    }
+
+    @Test
+    public void  handlePost() throws Exception {
+
+        BeerDto beerDto =validBeer;
+        beerDto.setId(null);
+
+        BeerDto savedDto = BeerDto.builder()
+                .id(UUID.randomUUID())
+                .beerName("New Beer")
+                .build();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+        given(beerService.saveNewBeer(any())).willReturn(savedDto);
+
+
+
+        mockMvc.perform(post("/api/v1/beer/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isCreated());
+
+        ArgumentCaptor<BeerDto> beerDtoCaptor = ArgumentCaptor.forClass(BeerDto.class);
+        verify(beerService,times(1))
+                .saveNewBeer(beerDtoCaptor.capture());
+        BeerDto dtoArgument = beerDtoCaptor.getValue();
+       assertThat(dtoArgument.getBeerName()).isEqualTo("Galaxy Cat");
+       assertThat(dtoArgument.getBeerStyle()).isEqualTo("Pale Ale");
+       assertThat(dtoArgument.getUpc()).isEqualTo(123456789012L);
 
     }
 }
